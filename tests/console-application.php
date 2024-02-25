1e562fd17f6d4a88962c8864afbaa64aad747960
Tomáš Reinert <reinerttomas@gmail.com>
2024-02-25 19:17:17 +0100

    install pint

--- tests/console-application.php
+++ tests/console-application.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 use App\Kernel;
@@ -10,4 +11,5 @@ require __DIR__ . '/../vendor/autoload.php';
 (new Dotenv())->bootEnv(__DIR__ . '/../.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+
 return new Application($kernel);
